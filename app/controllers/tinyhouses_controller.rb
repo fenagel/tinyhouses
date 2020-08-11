9560f582829eace7d8adbe29ef0ad83e7ef7e9f6
Dareos Khalili <khalilidareos@gmail.com>
2020-08-11 17:41:14 +0200

    adds index method and refactors controller

--- app/controllers/tinyhouses_controller.rb
+++ app/controllers/tinyhouses_controller.rb
@@ -1,6 +1,15 @@
 class TinyhousesController < ApplicationController
-  def show
+  before_action :find_tinyhouse, only: [:show]
+
+  def show; end
+
+  def index
+    @tinyhouses = Tinyhouse.all
+  end
+
+  private
+
+  def find_tinyhouse
     @tinyhouse = Tinyhouse.find(params[:id])
   end
-  
 end
