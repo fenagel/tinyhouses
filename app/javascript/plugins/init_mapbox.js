54903cac01c63232c4fa68cd339244a7a36fe4c0
Dareos Khalili <khalilidareos@gmail.com>
2020-08-26 12:06:11 +0200

    styles map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -17,7 +17,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/harveyswick/ckeb59cyi0xgd1amvgc45y43b'
+      style: 'mapbox://styles/harveyswick/ckeb63qqf0aqi19obtw1ef9w7'
     });
 
   const markers = JSON.parse(mapElement.dataset.markers);
