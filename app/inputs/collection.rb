8aec41b60471874e36734bcb17e5357c7cdb740c
Felix Nagel <felixnagel105@gmail.com>
2020-10-27 12:25:04 +0100

    trying to fix deployment error: uninitialized constant SimpleForm::Inputs::Collection

--- app/inputs/collection.rb
+++ app/inputs/collection.rb
@@ -1,5 +1,5 @@
-class Collection < SimpleForm::Inputs::Collection
-  def input(wrapper_options)
-    template.content_tag(:div, super)
-  end
-end
+# class Collection < SimpleForm::Inputs::Collection
+#   def input(wrapper_options)
+#     template.content_tag(:div, super)
+#   end
+# end
