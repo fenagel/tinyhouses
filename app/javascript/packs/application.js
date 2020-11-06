703a12d85b7dd00e2c34ec93ec141bdc30a00c91
Felix Nagel <felixnagel105@gmail.com>
2020-11-06 13:33:16 +0100

    changed import location for flatpickr import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-import { toggleDateInputs } from "../components/trip";
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -29,6 +27,7 @@ import "flatpickr/dist/flatpickr.min.css";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from "../plugins/init_mapbox";
+import { toggleDateInputs } from "../components/trip";
 
 // call your functions here so that they are called in every relevant page of the app.
 // && work with AJAX
