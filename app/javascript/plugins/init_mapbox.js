073f0aa8ed2293cacce3a2bb820e59726f1a0c64
Dareos Khalili <khalilidareos@gmail.com>
2021-11-18 11:04:20 +0100

    test

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -27,6 +27,7 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow.content);
+      // new mapboxgl.Marker({ "color": "red"}) you can pass options here. Check docs for more
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
